27f8c1acff7083b84f9d81880ae3a4e1f4311c2c
Ferdinand Ogar <116808690+webinand@users.noreply.github.com>
2023-10-13 11:24:18 +0100

    first page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="container">
+      <div className="header">
+        <p>CODE/AI</p>
+      </div>
+      <div className="label">
+        <p className="improve-your-code">
+          Improve Your Code, <br />
+          Empower Your Ability <br />
+          with AI
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="buttons">
+        <button className="improve">Let's Improve</button>
+        <button className="about">About</button>
+      </div>
     </div>
   );
 }
